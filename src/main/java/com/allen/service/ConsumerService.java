28c38e7831387ac1f7b9a76383c1972705a49876
liqiaochen <liqiaochen@infinite.net.cn>
2018-05-11 22:02:07 +0800

    代码更新

--- src/main/java/com/allen/service/ConsumerService.java
+++ src/main/java/com/allen/service/ConsumerService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class ConsumerService {
@@ -12,9 +13,19 @@ public class ConsumerService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod="ribbonAddFallback")//指定回调方法
-	public String ribbonAdd() throws InterruptedException{
-		Thread.sleep(5000L);
+	@HystrixCommand(
+			fallbackMethod="ribbonAddFallback",//指定回调方法
+			threadPoolProperties={
+					@HystrixProperty(name="coreSize",value="30"),
+					@HystrixProperty(name="maxQueueSize",value="100"),
+					@HystrixProperty(name="queueSizeRejectionThreshold",value="20")
+			},
+			commandProperties={
+					@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="100"),
+					@HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value="50")
+			}
+	)
+	public String ribbonAdd(){
 		return restTemplate.getForEntity("http://provider-service/add?a=10&b=20", String.class).getBody();
 	}
 	
